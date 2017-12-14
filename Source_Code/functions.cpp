b7385db597116d46513737f9dd1be96db3ee8765
Jesús Montoya <yiyismon.14@gmail.com>
2017-12-14 10:47:55 -0500

    Inline Functions

--- Source_Code/functions.cpp
+++ Source_Code/functions.cpp
@@ -10,6 +10,13 @@ int sum(int x, int y, int z) {
     return x + y + z;
 }
 
+//Inline Function, use this type for small pieces of code that are used frequently
+inline void swap(int &a, int &b){
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
 int main(void) {
     int x, y;
     scanf("%d %d", &x, &y);
