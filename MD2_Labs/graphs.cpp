62a8477c47c154cdfe0c7ca570f53dad7c8821c3
Jesús Montoya <yiyismon.14@gmail.com>
2017-12-29 10:31:25 -0500

    updated Graphs

--- MD2_Labs/graphs.cpp
+++ MD2_Labs/graphs.cpp
@@ -3,32 +3,31 @@
 using namespace std;
 
 int getDeg(int node, vector<vector<int>> edges) {
-    int d;
-    int size = sizeof(edges) / sizeof(edges[0]);
-    for (int i = 0; i < size; i++) {
+    int d = 0;
+    for (int i = 0; i < edges.size(); i++) {
         d += edges[node][i];
     }
     return d;
 }
+
 int getBucles(vector<vector<int>> _edg) {
-    int size = sizeof(_edg) / sizeof(_edg[0]);
     int bNum = 0;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < _edg.size(); i++) {
         if (_edg[i][i] == 2) {
             bNum += 1;
         }
     }
     return bNum;
 }
+
 char getLetter(vector<vector<int>> _edg) {
     char letter;
-    int size = sizeof(_edg) / sizeof(_edg[0]);
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    for (int i = 0; i < _edg.size(); i++) {
+        for (int j = 0; j < _edg.size(); j++) {
             if (_edg[i][j] == 1 && _edg[j][i] == 1) {
                 letter = 'S';
             }
-            else{
+            else {
                 letter = 'N';
             }
         }
@@ -47,12 +46,16 @@ int main() {
     scanf("%d", &casos);
     for (int i = 0; i < casos; i++) {
         scanf("%d", &nodos);
+        _edges.resize(nodos);
+        for (int l = 0; l < nodos; ++l) {
+            _edges[l].resize(nodos);
+        }
         scanf("%d", &edgesNum);
         for (int j = 0; j < edgesNum; j++) {
             scanf("%d %d", &start, &end);
             _edges[start][end] += 1;
         }
-        printf("%d\n", i);
+        cout << i + 1 << '\n';
         for (int k = 0; k < nodos; k++) {
             cout << getDeg(k, _edges) << ' ';
         }
