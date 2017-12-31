e19dca050c24c5f8e26269f962b7ac37317edd96
Jesús Montoya <yiyismon.14@gmail.com>
2017-12-31 06:42:40 -0500

    Mission 1 Completed!

--- MD2_Labs/graphs.cpp
+++ MD2_Labs/graphs.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int getDeg(int node, vector<vector<int>>& edges) {
+int getDeg(int node, vector<vector<int>> &edges) {
     int d = 0;
     for (int i = 0; i < edges.size(); i++) {
         d += edges[node][i];
@@ -10,25 +10,13 @@ int getDeg(int node, vector<vector<int>>& edges) {
     return d;
 }
 
-int getBucles(vector<vector<int>>& _edg) {
-    int bNum = 0;
+char getLetter(vector<vector<int>> &_edg) {
+    char letter = 'N';
     for (int i = 0; i < _edg.size(); i++) {
-        if (_edg[i][i] == 2) {
-            bNum += 1;
-        }
-    }
-    return bNum;
-}
-
-char getLetter(vector<vector<int>>& _edg) {
-    char letter;
-    for (int i = 0; i < _edg.size(); i++) {
-        for (int j = 0; j < _edg.size(); j++) {
-            if (_edg[i][j] == 1 && _edg[j][i] == 1) {
+        for (int j = (i + 1); j < _edg.size(); j++) {
+            if (_edg[i][j] >= 2) {
                 letter = 'S';
-            }
-            else {
-                letter = 'N';
+                return letter;
             }
         }
     }
@@ -43,25 +31,58 @@ int main() {
     int start;
     int end;
 
-    scanf("%d", &casos);
+    ofstream afile("result.txt", ios::out);
+    FILE *data;
+    char name[25];
+    scanf("%s", name);
+    data = fopen(name, "r");
+    if (data == NULL) {
+        cout << "Error de apertura del archivo";
+        exit (1);
+    }
+
+    fscanf(data, "%d", &casos);
     for (int i = 0; i < casos; i++) {
-        scanf("%d", &nodos);
+        int bucles = 0;
+        fscanf(data, "%d", &nodos);
         _edges.resize(nodos);
         for (int l = 0; l < nodos; ++l) {
             _edges[l].resize(nodos);
         }
-        scanf("%d", &edgesNum);
-        for (int j = 0; j < edgesNum; j++) {
-            scanf("%d %d", &start, &end);
-            _edges[start][end] += 1;
-        }
-        cout << i + 1 << '\n';
         for (int k = 0; k < nodos; k++) {
-            cout << getDeg(k, _edges) << ' ';
+            for (int l = 0; l < nodos; l++) {
+                _edges[k][l] = 0;
+            }
         }
-        cout << '\n';
-        cout << getBucles(_edges) << '\n';
-        cout << getLetter(_edges) << '\n';
+        fscanf(data, "%d", &edgesNum);
+        for (int j = 0; j < edgesNum; j++) {
+            fscanf(data, "%d %d", &start, &end);
+            _edges[start - 1][end - 1]++;
+            _edges[end - 1][start - 1]++;
+            if (start == end) {
+                bucles++;
+            }
+        }
+        afile << i + 1 << '\n';
+        for (int k = 0; k < nodos; k++) {
+            if (k == nodos - 1) {
+                afile << getDeg(k, _edges);
+            }
+            else {
+                afile << getDeg(k, _edges) << ' ';
+            }
+
+        }
+        afile << '\n';
+        afile << bucles << '\n';
+        if (i == casos - 1) {
+            afile << getLetter(_edges);
+        }
+        else {
+            afile << getLetter(_edges) << '\n';
+        }
+
     }
+    fclose(data);
     return 0;
 }
