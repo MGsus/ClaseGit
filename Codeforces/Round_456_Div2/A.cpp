361649ee8f2659e41883ce7c45c83bf3c67559fb
Jesús Montoya <yiyismon.14@gmail.com>
2018-01-05 17:42:36 -0500

    912 Tricky Alchemy

--- Codeforces/Round_456_Div2/A.cpp
+++ Codeforces/Round_456_Div2/A.cpp
@@ -44,16 +44,14 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-long long int yellowC, blueC, yellowB, greenB, blueB, res;
+typedef long long ll;
 
 int main() {
+    ll yellowC, yellowB, blueB, blueC, greenB;
+    scanf("%lld %lld", &yellowC, &blueC);
+    scanf("%lld %lld %lld", &yellowB, &greenB, &blueB);
 
-    scanf("%d %d", &yellowC, &blueC);
-    scanf("%d %d %d", &yellowB, &greenB, &blueB);
-
-    res = max(0LL, 2 * yellowB + greenB - yellowC) + max(0LL, 3 * blueB +
-                                                         greenB - blueC);
+    res = max(0LL, 2 * yellowB + greenB - yellowC) + max(0LL, 3 * blueB + greenB - blueC);
     cout << res;
     return 0;
 }
