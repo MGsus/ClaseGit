6f119387b473bb0dc08ea133d9b6a850ebbce224
Jesús Montoya <yiyismon.14@gmail.com>
2018-01-05 13:25:18 -0500

    Counting Carts

---
+++ Codeforces/Codeforces Good Bye 2017 Contest/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    string input;
+    int res = 0;
+    cin >> input;
+    for (int i = 0; i < input.length(); i++) {
+        if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u') {
+            res++;
+        }
+        if (input[i] == '1' || input[i] == '3' || input[i] == '5' || input[i] == '7' || input[i] == '9') {
+            res++;
+        }
+    }
+    cout << res;
+    return 0;
+}
