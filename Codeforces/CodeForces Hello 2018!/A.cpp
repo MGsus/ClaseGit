b974b2d54a2514fc39bf9214a5c48e42ef3dfeb7
Jesús Montoya <yiyismon.14@gmail.com>
2018-01-08 14:10:51 -0500

    Modular Exponentiation

--- Codeforces/CodeForces Hello 2018!/A.cpp
+++ Codeforces/CodeForces Hello 2018!/A.cpp
@@ -7,7 +7,10 @@ int m;
 
 int main( ) {
     scanf("%d %d", &n, &m);
-    if (n >= 30)
-        return printf("%d", m);
-    return printf("%d", m % (1 << n)); // (1 << n) == 2 a la n
+    if (n >= 30) {
+        printf("%d", m);
+        return 0;
+    }
+    printf("%d", m % (1 << n)); // (1 << n) == 2 a la n
+    return 0;
 }
