158923620a6719ea03bbfca08980185bfa307df4
Jesús Montoya <yiyismon.14@gmail.com>
2018-03-28 07:10:04 -0500

    .

---
+++ Submit Error UPB/630D Hexagons.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+ll int n;
+
+int main(int argc, char const *argv[]) {
+    cin >> n;
+    cout << 1 + 3 * n * (n + 1);
+    return 0;
+}
---
+++ Submit Error UPB/630R Game.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+ll int n;
+
+int main(int argc, char const *argv[]) {
+    cin >> n;
+    if (n % 2 == 0)
+        cout << 2;
+    else
+        cout << 1;
+    return 0;
+}
