967be05629ed6d4a5149c96686f66cd3a33370ff
Jesús Montoya <yiyismon.14@gmail.com>
2018-08-18 10:25:47 -0500

    .

--- Submit Error UPB/a2oj_346.cpp
+++ Submit Error UPB/a2oj_346.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 float a, b, c;
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
     cin >> a >> b >> c;
     float res = (a + b + c) + (a + b + c);
     printf("%0.6f", res);
