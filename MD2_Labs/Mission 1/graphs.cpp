54b91b09b9a6058c33f4cd3486888cd8f952ebd9
Jesús Montoya <yiyismon.14@gmail.com>
2018-08-20 12:25:35 -0500

    .

--- MD2_Labs/Mission 1/graphs.cpp
+++ MD2_Labs/Mission 1/graphs.cpp
@@ -2,19 +2,25 @@
 
 using namespace std;
 
-int getDeg(int node, vector<vector<int>> &edges) {
+int getDeg(int node, vector<vector<int>> &edges)
+{
     int d = 0;
-    for (int i = 0; i < edges.size(); i++) {
+    for (int i = 0; i < edges.size(); i++)
+    {
         d += edges[node][i];
     }
     return d;
 }
 
-char getLetter(vector<vector<int>> &_edg) {
+char getLetter(vector<vector<int>> &_edg)
+{
     char letter = 'N';
-    for (int i = 0; i < _edg.size(); i++) {
-        for (int j = (i + 1); j < _edg.size(); j++) {
-            if (_edg[i][j] >= 2) {
+    for (int i = 0; i < _edg.size(); i++)
+    {
+        for (int j = (i + 1); j < _edg.size(); j++)
+        {
+            if (_edg[i][j] >= 2)
+            {
                 letter = 'S';
                 return letter;
             }
@@ -23,7 +29,8 @@ char getLetter(vector<vector<int>> &_edg) {
     return letter;
 }
 
-int main() {
+int main()
+{
     int casos;
     vector<vector<int>> _edges;
     int edgesNum;
@@ -31,58 +38,59 @@ int main() {
     int start;
     int end;
 
-    ofstream afile("result.txt", ios::out);
-    FILE *data;
-    char name[25];
-    scanf("%s", name);
-    data = fopen(name, "r");
-    if (data == NULL) {
-        cout << "Error de apertura del archivo";
-        exit (1);
-    }
+    freopen("grafos-easy-in.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
-    fscanf(data, "%d", &casos);
-    for (int i = 0; i < casos; i++) {
+    scanf("%d", &casos);
+    for (int i = 0; i < casos; i++)
+    {
         int bucles = 0;
-        fscanf(data, "%d", &nodos);
+        scanf("%d", &nodos);
         _edges.resize(nodos);
-        for (int l = 0; l < nodos; ++l) {
+        for (int l = 0; l < nodos; ++l)
+        {
             _edges[l].resize(nodos);
         }
-        for (int k = 0; k < nodos; k++) {
-            for (int l = 0; l < nodos; l++) {
+        for (int k = 0; k < nodos; k++)
+        {
+            for (int l = 0; l < nodos; l++)
+            {
                 _edges[k][l] = 0;
             }
         }
-        fscanf(data, "%d", &edgesNum);
-        for (int j = 0; j < edgesNum; j++) {
-            fscanf(data, "%d %d", &start, &end);
+        scanf("%d", &edgesNum);
+        for (int j = 0; j < edgesNum; j++)
+        {
+            scanf("%d %d", &start, &end);
             _edges[start - 1][end - 1]++;
             _edges[end - 1][start - 1]++;
-            if (start == end) {
+            if (start == end)
+            {
                 bucles++;
             }
         }
-        afile << i + 1 << '\n';
-        for (int k = 0; k < nodos; k++) {
-            if (k == nodos - 1) {
-                afile << getDeg(k, _edges);
+        cout << i + 1 << '\n';
+        for (int k = 0; k < nodos; k++)
+        {
+            if (k == nodos - 1)
+            {
+                cout << getDeg(k, _edges);
             }
-            else {
-                afile << getDeg(k, _edges) << ' ';
+            else
+            {
+                cout << getDeg(k, _edges) << ' ';
             }
-
         }
-        afile << '\n';
-        afile << bucles << '\n';
-        if (i == casos - 1) {
-            afile << getLetter(_edges);
+        cout << '\n';
+        cout << bucles << '\n';
+        if (i == casos - 1)
+        {
+            cout << getLetter(_edges);
         }
-        else {
-            afile << getLetter(_edges) << '\n';
+        else
+        {
+            cout << getLetter(_edges) << '\n';
         }
-
     }
-    fclose(data);
     return 0;
 }
--- Submit Error UPB/a2oj_137.cpp
+++ Submit Error UPB/a2oj_137.cpp
@@ -2,14 +2,35 @@
 
 using namespace std;
 
-int t, m, n;
+int t, n, m;
+string const LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string s;
+char c;
 
-
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     cin >> t;
-    for (int i = 0; i < t; i++) {
+
+    for (int i = 0; i < t; i++)
+    {
         cin >> n >> m;
 
+        for (int j = 0; j < n; i++)
+        {
+            cin >> s;
+            while (s[j])
+            {
+                c = s[j];
+                if (islower(c))
+                    s[j] = toupper(c);
+                else
+                    s[j] = tolower(c);
+                i++;
+            }
+            cout << s << '\n';
+        }
     }
     return 0;
 }
