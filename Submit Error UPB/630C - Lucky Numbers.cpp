2a4cb733cc8b5c537a653b4d7843d91541d26e35
Jesús Montoya <yiyismon.14@gmail.com>
2018-09-07 13:35:09 -0500

    .

--- Submit Error UPB/630C - Lucky Numbers.cpp
+++ Submit Error UPB/630C - Lucky Numbers.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 ll int n;
 
-int main( ) {
+int main(int argc, char const* argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     cin >> n;
     std::cout << (2LL << n) - 2;
     return 0;
