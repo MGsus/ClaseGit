710d87c0b0e47a57524c8f599fdcaa9433c2bd66
Jesús Montoya <yiyismon.14@gmail.com>
2018-09-09 20:41:48 -0500

    .

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
 # By **_Gsus_**
+
 **C++ Code use in competitive programing :D**
 
 Copyright (c) 2018 Copyright Holder All Rights Reserved.
--- Submit Error UPB/a2oj_155.cpp
+++ Submit Error UPB/a2oj_155.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
-#define MOD 33554431
+
 using namespace std;
 
 int t, n, res;
+const int MOD = 33554431;
 
 int main(int argc, char const *argv[])
 {
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false); // Optimize Code
     cin.tie(0);
     cin >> t;
+
     for (int i = 0; i < t; i++)
     {
         cin >> n;
-        res = ((1 << (n + 1)) - 1) / (2 - 1) % MOD;
-        cout << res;
+        res = ((1 << (n + 1)) - 1) / (2 - 1);
+	cout << res % MOD << '\n';
     }
     return 0;
 }
